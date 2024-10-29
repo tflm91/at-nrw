dc6e724e272b50400433b68e19f19ed7655d0881
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-10-29 14:33:57 +0100

    add shortcode for aids page

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -1,10 +1,11 @@
 <?php
 
 /**
- * Shortcode to display disabilities
+ * List of all available shortcodes
  */
 
 
 require_once get_stylesheet_directory() . '/shortcodes/show_disabilities.php';
 require_once get_stylesheet_directory() . '/shortcodes/show_limitations.php';
+require_once get_stylesheet_directory() . '/shortcodes/show_aids.php';
 ?>
---
+++ shortcodes/show_aids.php
@@ -0,0 +1,8 @@
+<?php
+/* the shortcode for displaying the assistive technologies */
+function show_aids() {
+    return "Diese Seite ist in Arbeit und wird bald für Sie verfügbar sein. ";
+}
+
+add_shortcode("aids", "show_aids");
+?>
