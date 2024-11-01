ce89135737c81bec569b547b3a1abbba47a0c7f5
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-11-01 11:19:24 +0100

    fix retrievement of category-IDs in links

--- inc/product_category.php
+++ inc/product_category.php
@@ -4,7 +4,7 @@ function list_product_categories($connection_table_name, $impairment_id) {
     global $wpdb;
     $category_table_name = ASSISTIVE_TECHNOLOGY_CATEGORY_TABLE;
 
-    $stmt = "SELECT $category_table_name.name AS name FROM $connection_table_name"
+    $stmt = "SELECT $category_table_name.id AS id, $category_table_name.name AS name FROM $connection_table_name"
         . " INNER JOIN $category_table_name ON $connection_table_name.assistiveTechnologyCategoryId = $category_table_name.id"
         . " WHERE $connection_table_name.impairmentId = %d";
 
