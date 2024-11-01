702c71cd92774f80cb8a355ad79018b5f0abffa0
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-11-01 20:29:45 +0100

    fix university shortcode

--- shortcodes/show_universities.php
+++ shortcodes/show_universities.php
@@ -1,7 +1,7 @@
 <?php
 /* the shortcodes for displaying the universities */
-function show_universities() {} {
-    return "Diese Seite ist in Arbeit und wird bald für Sie verfügbar sein. ";
+function show_universities() {
+    return "Diese Seite ist in Arbeit und wird bald für Sie verfügbar sein.";
 }
 
 add_shortcode("universities", "show_universities");
