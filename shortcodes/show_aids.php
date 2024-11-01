803390d4133e994c21de091c946a59e97f384799
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-11-01 16:10:28 +0100

    list products for each category

--- shortcodes/show_aids.php
+++ shortcodes/show_aids.php
@@ -5,12 +5,31 @@ require_once get_stylesheet_directory() . "/table-names.php";
 function show_aids() {
     global $wpdb;
     $product_categories_table_name = ASSISTIVE_TECHNOLOGY_CATEGORY_TABLE;
+    $connection_table_name = CATEGORY_OF_PRODUCT_TABLE;
+    $product_table_name = PRODUCT_TABLE;
+
     $disability_categories = $wpdb->get_results("SELECT * FROM $product_categories_table_name");
     $output = "<div>\n";
     if ($disability_categories) {
         foreach ($disability_categories as $category) {
             $heading_id = $category->id;
             $output .= "<h2 id='category-" . $heading_id . "'>" . esc_html($category->name) . "</h2>\n";
+
+            $stmt = "SELECT $product_table_name.id AS id, $product_table_name.name AS name FROM $connection_table_name"
+                . " INNER JOIN $product_table_name ON $connection_table_name.productId = $product_table_name.id"
+                . "WHERE $connection_table_name.categoryId = %d";
+
+            $products = $wpdb->get_results($wpdb->prepare($stmt, $category->id));
+
+            if ($products) {
+                $output .= "<ul>\n";
+                foreach ($products as $product) {
+                    $output .= "<li>" . $product->name . "</li>\n";
+                }
+                $output .= "</ul>\n";
+            } else {
+                $output .= "<p>Keine passenden Produkte gefunden.</p>\n";
+            }
         }
     } else {
         $output .= "<p>Keine Hilfsmittel vorhanden</p>\n";
