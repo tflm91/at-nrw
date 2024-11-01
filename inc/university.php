84dcd93cb07827e205d6a8f2f3fb96aea828d91b
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-11-01 21:22:55 +0100

    fix contact links

--- inc/university.php
+++ inc/university.php
@@ -4,7 +4,7 @@
 function show_university_information($university) {
     $output = "<p><b>Kontaktinformationen zur Beratungsstelle für behinderte Studierende:</b><br />\n";
     $output .= nl2br($university->contactInformation) . "</p>\n";
-    $output .= "<p><b>Link zur Beratungsstelle: </b><a href='" . esc_url($university->contactURL) . "'>". esc_url($university->contactAlt) . "</a></p>\n";
+    $output .= "<p><b>Link zur Beratungsstelle: </b><a href='" . esc_url($university->contactURL) . "'>". esc_html($university->contactAlt) . "</a></p>\n";
     $output .= "<p><b>Arbeitsräume: </b>" . esc_html($university->workspaces) . "</p>\n";
     return $output;
 }
