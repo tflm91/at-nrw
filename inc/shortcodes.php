1c63631e7079f09aa7936bbdc0ed1a2383ae436c
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-11-01 20:23:17 +0100

    add shortcode for university

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -8,4 +8,5 @@
 require_once get_stylesheet_directory() . '/shortcodes/show_disabilities.php';
 require_once get_stylesheet_directory() . '/shortcodes/show_limitations.php';
 require_once get_stylesheet_directory() . '/shortcodes/show_aids.php';
+require_once get_stylesheet_directory() . '/shortcodes/show_universities.php';
 ?>
---
+++ shortcodes/show_universities.php
@@ -0,0 +1,8 @@
+<?php
+/* the shortcodes for displaying the universities */
+function show_universities() {} {
+    return "Diese Seite ist in Arbeit und wird bald für Sie verfügbar sein. ";
+}
+
+add_shortcode("universities", "show_universities");
+?>
