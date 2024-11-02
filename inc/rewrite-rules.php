4a5886a4818de59844f759fcf6aab50465f1a793
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-11-02 20:40:15 +0100

    refactor

--- inc/rewrite-rules.php
+++ inc/rewrite-rules.php
@@ -1,20 +1,30 @@
+
 <?php
 /**
- * Add custom query variables
+ * Registers custom query variables for specific page routing.
+ *
+ * @param array $vars Existing query variables.
+ * @return array Modified array of query variables.
  */
-function custom_query_vars($vars) {
+function atnrw_custom_query_vars($vars) {
     $vars[] = 'disability_id';
     $vars[] = 'product_id';
     $vars[] = 'university_id';
     return $vars;
 }
-add_filter('query_vars', 'custom_query_vars');
+add_filter('query_vars', 'atnrw_custom_query_vars');
 
-
-function custom_rewrite_rules() {
+/**
+ * Adds custom rewrite rules to handle specific URL structures.
+ *
+ * This function maps custom URLs to corresponding templates and query variables.
+ *
+ * @return void
+ */
+function atnrw_custom_rewrite_rules() {
     add_rewrite_rule('^behinderungen/([0-9]+)/?', 'index.php?pagename=behinderungen&disability_id=$matches[1]', 'top');
     add_rewrite_rule('^hilfsmittel/([0-9]+)/?', 'index.php?pagename=hilfsmittel&product_id=$matches[1]', 'top');
     add_rewrite_rule('^hochschulen/([0-9]+)/?', 'index.php?pagename=hochschulen&university_id=$matches[1]', 'top');
 }
-add_action( 'init', 'custom_rewrite_rules' );
+add_action('init', 'atnrw_custom_rewrite_rules');
 ?>
