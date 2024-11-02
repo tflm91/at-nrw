0190ab108b7e388930cf07ee596eba2c563525c4
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2024-11-02 12:08:43 +0100

    fix university URL structure

--- inc/rewrite-rules.php
+++ inc/rewrite-rules.php
@@ -14,7 +14,7 @@ add_filter('query_vars', 'custom_query_vars');
 function custom_rewrite_rules() {
     add_rewrite_rule('^behinderungen/([0-9]+)/?', 'index.php?pagename=behinderungen&disability_id=$matches[1]', 'top');
     add_rewrite_rule('^hilfsmittel/([0-9]+)/?', 'index.php?pagename=hilfsmittel&product_id=$matches[1]', 'top');
-    add_rewrite_rule('^hochschulen/([0-9]+)/?', 'index.php?pagename=hochschulen&product_id=$matches[1]', 'top');
+    add_rewrite_rule('^hochschulen/([0-9]+)/?', 'index.php?pagename=hochschulen&university_id=$matches[1]', 'top');
 }
 add_action( 'init', 'custom_rewrite_rules' );
 ?>
