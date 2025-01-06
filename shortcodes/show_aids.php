025260dba7e9e38d8fe017117dc8115388377973
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-01-06 10:01:40 +0100

    remove back-link to aids page

--- shortcodes/show_aids.php
+++ shortcodes/show_aids.php
@@ -101,8 +101,8 @@ function show_detailed_product_information($wpdb, $product_id) {
     } else {
         $output .= "<p>Dieses Produkt wurde nicht gefunden. </p>\n";
     }
-    $back_url = site_url('/hilfsmittel');
-    $output .= "<a href='". $back_url ."' aria-label='Zurück zur Seite mit der Übersicht aller verfügbaren Hilfsmittel'>Zur Übersicht aller Hilfsmittel</a>\n";
+    /* $back_url = site_url('/hilfsmittel');
+    $output .= "<a href='". $back_url ."' aria-label='Zurück zur Seite mit der Übersicht aller verfügbaren Hilfsmittel'>Zur Übersicht aller Hilfsmittel</a>\n"; */
     $output .= "</div>\n";
     return $output;
 }
