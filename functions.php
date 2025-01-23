2ce95d16c790d3374334f3c64cee65bf16028997
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-01-23 16:58:53 +0100

    logout without confirmation

--- functions.php
+++ functions.php
@@ -3,4 +3,5 @@ require_once get_stylesheet_directory() . '/inc/enqueue-scripts.php';
 require_once get_stylesheet_directory() . '/inc/rewrite-rules.php';
 require_once get_stylesheet_directory() . '/inc/shortcodes.php';
 require_once get_stylesheet_directory() . '/inc/footer.php';
+require_once get_stylesheet_directory() . '/inc/logout.php';
 ?>
