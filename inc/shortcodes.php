c3c7e8ad1ede5e708c69552a97c1a08e816dc571
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-01-28 12:49:56 +0100

    insert delete account shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -9,4 +9,5 @@ require_once get_stylesheet_directory() . '/shortcodes/show_disabilities.php';
 require_once get_stylesheet_directory() . '/shortcodes/show_limitations.php';
 require_once get_stylesheet_directory() . '/shortcodes/show_aids.php';
 require_once get_stylesheet_directory() . '/shortcodes/show_universities.php';
+require_once get_stylesheet_directory() . '/shortcodes/delete-account.php';
 ?>
