235633db43e0114ed7116e88ccfb63aa0b2c4929
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-01-28 20:51:16 +0100

    insert debug logs

--- shortcodes/delete-account.php
+++ shortcodes/delete-account.php
@@ -26,24 +26,26 @@ function delete_account_js() {
 add_action('wp_footer', 'delete_account_js');
 
 function delete_user_account() {
+    error_log("delete_user_account() wurde aufgerufen..."); 
     if ( is_user_logged_in()) {
         $user_id = get_current_user_id();
         $user = get_user_by('id', $user_id);
 
         if (in_array('administrator', (array) $user->roles)) {
             wp_redirect(home_url('konto-loeschen-fehlgeschlagen'));
-        } else {
-            wp_redirect(home_url('konto-geloescht'));
+            exit;
         }
 
-/*        wp_logout();
+        wp_logout();
 
         require_once ( ABSPATH . 'wp-admin/includes/user.php' );
         wp_delete_user($user_id);
-*/
 
+        wp_redirect(home_url('konto-geloescht'));
+        exit;
     } else {
-        wp_redirect(home_url('nicht-eingeloggt'));
+        wp_redirect(home_url());
+        exit;
     }
 }
 
