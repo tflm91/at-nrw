a656cc5b0e073aa301d076d68a15035587fdf82e
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-02-09 18:03:20 +0100

    document functions of helpers.php

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,14 +1,18 @@
 <?php
+/* generate a link list of different items */
 function generate_item_list($items, $subpage, $before_html = null, $error = null, $id_prefix = null): string {
     $output = "";
     if($items) {
         if($before_html) {
             $output .= $before_html;
         }
+
         $output .= "<ul>";
+
         foreach($items as $item) {
             $output .= '<li>' . generate_item_link($item, $subpage, $id_prefix) . '</li>';
         }
+
         $output .= "</ul>\n";
     } else {
         if ($error) {
@@ -19,6 +23,7 @@ function generate_item_list($items, $subpage, $before_html = null, $error = null
     return $output;
 }
 
+/* generate link to a specific item for link list */
 function generate_item_link($item, $subpage, $id_prefix = null): string {
     $url = site_url(
         '/'
